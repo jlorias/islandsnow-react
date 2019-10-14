b99b10e9e793838227abc975c90b496abd7e3ca8
jlorias <jlorias@hawaii.edu>
2019-10-14 13:13:46 -1000

    Task 3
    Task Trois

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class IslandSnow extends React.Component {
 
   render() {
     return (
-        <h1>Island Snow!</h1>
-  );
+        <Container textAlign="center">
+          <Header as='h1'>Island Snow!</Header>
+        </Container>
+    );
   }
 }
 
-ReactDOM.render(<IslandSnow />, document.getElementById('root'));
+ReactDOM.render(<IslandSnow/>, document.getElementById('root'));
